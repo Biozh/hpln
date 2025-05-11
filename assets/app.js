2fee1a2a2a20da26d8241107eab07efbc5be375a
Hector <hector.bidan@gmail.com>
2025-05-11 19:35:30 +0200

    maj fonts

--- assets/app.js
+++ assets/app.js
@@ -6,7 +6,7 @@
  */
 
 // any CSS you import will output into a single css file (app.css in this case)
-import 'material-symbols';
+// import 'material-symbols';
 import './styles/app.scss';
 
 import $ from 'jquery';
